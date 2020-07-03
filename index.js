fe8f34d30699f48096413e20b1306d0c285d4a2b
Wesley Gimenez <50761161+wesleymtx@users.noreply.github.com>
2020-07-03 20:55:22 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const automate = ()=>{
+    let numero = Math.random()*10
+    if(numero>5)
+        like = document.querySelector('[aria-label="Gosto"]').click()
+    else
+    deslike = document.querySelector('[aria-label=Não]').click()
+}
+setInterval(automate,5000)
